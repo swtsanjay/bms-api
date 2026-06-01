6acbe5399b8e5cccd049eb5100096cd909d8978f
= <=>
2026-06-01 22:09:57 +0530

    cc

---
+++ customer_referal_codes.rb
@@ -0,0 +1,16 @@
+customer_referal_codes
+
+id
+old_customer_id
+new_customer_id
+earned_coins
+expired_at
+status(created, expired)
+created_at
+updated_at
+
+
+
+customers
+total_earned_coins
+add new column referal_code
